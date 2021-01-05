3aaa9754b4f50662e918fba40a746473360f40cd
Alex <alexmil2004@mail.ru>
2021-01-05 21:18:23 +0300

    04

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function info(){
+
+        $user = Auth::user();
+
+        return $user;
+    }
+
+    public function bookings() {
+
+        return response()->json([],200);
+    }
+}
